9cd32e3beddfdc81bb4e2f89f0e2c9afedfed50e
Farrukhbek <68499298+1Farrukhbek@users.noreply.github.com>
2020-11-17 16:50:52 +0500

    Code optimization

--- js/utilities.js
+++ js/utilities.js
@@ -5,10 +5,10 @@ var $$_ = function(selector, node = document) {
   return node.querySelectorAll(selector);
 }
 
-
 var createElement = function(tagName, className, text) {
   var element = document.createElement(tagName);
   element.setAttribute('class', className);
+  
   if (text) {
     element.textContent = text;
   }
